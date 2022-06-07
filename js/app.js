5e00588479ac00cc7d6d8f5d5d2d668707b162c2
CoolRy <that007guy@hotmail.com>
2022-06-07 09:54:50 -0400

    added query selector buttons

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const git = document.querySelector('#git')
+const linked = document.querySelector('#linked')
+const mail = document.querySelector('#mail')
+const work = document.querySelector('#work')
+const contact = document.querySelector('#contact')
+const resume = document.querySelector('#resume')
+
