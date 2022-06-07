5185bd9c4ce8b3b6a2ed52b63d1ff29e46f7bf03
CoolRy <that007guy@hotmail.com>
2022-06-07 17:07:32 -0400

    added audio and social links

--- js/app.js
+++ js/app.js
@@ -4,4 +4,14 @@ const mail = document.querySelector('#mail')
 const work = document.querySelector('#work')
 const contact = document.querySelector('#contact')
 const resume = document.querySelector('#resume')
+const paragraph = document.querySelector('#paragraph')
+
+const allido = new Audio('../audio/allido.mp3')
+
+paragraph.addEventListener('click', function(){
+  allido.volume = .1
+  allido.play()
+})
+
+
 
