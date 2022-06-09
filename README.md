755e592403fba4b4587a53a6b42e195304791da8
CoolRy <that007guy@hotmail.com>
2022-06-09 15:40:53 -0400

    last minute cleanup

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Ryan-Portfolio
+
+This application is to showcase my beginning work as a software developer.  It shows basic use of HTML, CSS(all built from scratch minus bootstrap button) and Javascript while being able to learn a lttile bit more about Ryan.
+
+click link to launch https://coolry1031ryan-portfolio.netlify.app/
+
+Story Board credited through https://trello.com/b/wfLmnJcT/ryan-cooley-portfolio
+
 letter animation is credited through, https://tobiasahlin.com/moving-letters/#16
+
+project deployment link is credited through, https://getbootstrap.com/docs/5.1/components/dropdowns/
+
+Next planned steps: 
+- add animations of HTML CSS and JavaScript to make the bottom more fun.
+- continously update projects when they are completed
+- update the about me section to be a bit more professional
+- add a more professional photo of myself
+- update resume and linked profile
--- js/app.js
+++ js/app.js
@@ -1,19 +1,15 @@
 import { projectData } from "./project-data.js"
-console.log(projectData)
 
 const footer = document.getElementById('footer')
 const paragraph = document.getElementById('paragraph')
 const lightDarkBtn = document.getElementById('light-dark-button')
 const favicon = document.getElementById('favicon')
 const cardContainer = document.getElementById('dropdown')
-console.log(cardContainer)
-
-
 
 let projectMarkup = projectData.map(project =>
   ` <div class="dropdown">
   <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-    Dropdown link
+    Projects
   </a>
 
   <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
@@ -38,7 +34,6 @@ function changeFavicon () {
   favicon.setAttribute('href', '/images/Braves-Logo.png') 
 }
   
-
 // function revertFavicon () {
 //   favicon.setAttribute('href', '/images/Ryan.jpeg')
 // }
@@ -48,16 +43,6 @@ lightDarkBtn.addEventListener('click', toggleLightDark)
 function toggleLightDark() {
   paragraph.className = paragraph.className === 'dark' ? '' : 'dark'
 }
-function checkDarkPref() {
-  if (
-    window.matchMedia("(prefers-color-scheme:dark)").matches &&
-    body.className !== "dark"
-  ) {
-    toggleLightDark()
-  
-  }
-}
-checkDarkPref()
 
 anime.timeline({loop: true})
   .add({
