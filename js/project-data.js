a431b2e4baa5f4f8d1dfc8bd725a856a8cd4f15b
CoolRy <that007guy@hotmail.com>
2022-06-30 09:58:05 -0400

    add 2nd project

--- js/project-data.js
+++ js/project-data.js
@@ -6,6 +6,13 @@ let projectData = [
     github: 'https://github.com/CoolRy1031/ttt-weekend',
     description: 'A simple game of Tic-Tac Toe with a Braves Theme',
   },
+  {
+    title: 'Disc-Golf App',
+    deployment: 'https://coolry1031discgolf.herokuapp.com/',
+    image: '/images/disc-golf.png',
+    github: 'https://github.com/CoolRy1031/Disc-golf-app',
+    description: 'An app where you can create,review and add scores to a courses that you play.'
+  },
 ]
 
 export {
